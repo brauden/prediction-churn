429e1548b027b8d7f4bae6005d8d7b63661bf358
Dauren Bizhanov <dauren.bizhanov@duke.edu>
2022-10-25 18:02:12 -0400

    FCNN architecture for tabular data

--- src/models.py
+++ src/models.py
@@ -1,3 +1,21 @@
 """
 File for models definitions.
 """
+import torch
+from torch import nn
+
+
+class NewsFCNN(nn.Module):
+    def __init__(self):
+        super(NewsFCNN, self).__init__()
+        self.fc_sequence = nn.Sequential(
+            nn.Linear(58, 16),
+            nn.ReLU(),
+            nn.Linear(16, 16),
+            nn.ReLU(),
+            nn.Linear(16, 2)
+        )
+
+    def forward(self, x):
+        logits = self.fc_sequence(x)
+        return logits
